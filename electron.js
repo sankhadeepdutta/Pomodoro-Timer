f47810265073bed73928ee1e2e2341576d905d87
Sankhadeep Dutta <dutta.shankhadeep@gmail.com>
2025-05-19 12:24:30 +0530

    Updates

---
+++ electron.js
@@ -0,0 +1,29 @@
+const { app, BrowserWindow } = require("electron");
+const path = require("path");
+
+function createWindow() {
+  const win = new BrowserWindow({
+    width: 800,
+    height: 1200,
+    webPreferences: {
+      contextIsolation: true,
+    },
+  });
+
+  win.loadURL(
+    process.env.ELECTRON_START_URL ||
+      `file://${path.join(__dirname, "../build/index.html")}`
+  );
+}
+
+app.whenReady().then(() => {
+  createWindow();
+
+  app.on("activate", () => {
+    if (BrowserWindow.getAllWindows().length === 0) createWindow();
+  });
+});
+
+app.on("window-all-closed", () => {
+  if (process.platform !== "darwin") app.quit();
+});
